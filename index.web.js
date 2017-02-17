c5f3fee9d4156dc15c59494aebbbd6e0c70e7b7a
reggi <thomas@reggi.com>
2017-02-17 18:09:21 -0500

    react-native-web

---
+++ index.web.js
@@ -0,0 +1,6 @@
+import {AppRegistry} from 'react-native'
+import './src/main'
+
+AppRegistry.runApplication('AppPlayground', {
+  rootTag: document.getElementById('react-root')
+});
---
+++ web/react-native-web.js
@@ -0,0 +1,6 @@
+var RN = require('react-native-web')
+var RNV = require('@reggi/react-navigation-views')
+
+RN.NavigationExperimental = RNV
+
+module.exports = RN
